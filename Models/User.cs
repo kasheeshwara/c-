0a94eb2887e2c5b2623e47d795d97c4a354dbeba
Kasheeshwara reddy Chegireddy <kasheeshwarareddy@gmail.com>
2023-02-16 23:01:52 +0530

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Service.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            ServiceRequests = new HashSet<ServiceRequest>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public decimal Mobile { get; set; }
+        public DateTime RegisteredDate { get; set; }
+
+        public virtual ICollection<ServiceRequest> ServiceRequests { get; set; }
+    }
+}
